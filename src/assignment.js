fe59dab21049a768ea87854c200bbbb0ccae9a72
Damilola_J J <96253833+Djeneral007@users.noreply.github.com>
2022-07-20 02:02:18 +0000

    For test-challenge-1: I added a let function to sum 0. Then a for funtion for the arrayOfNumbers Then I summed the sum and item, and finally returned the function sum. Removed the comment function from the assignment. For test-challenge-2: I created a let function of newArray, then created a for function of the evenNumbers of the arrayOfNumbers, then returned a count function. Removed the comment from the assignment.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers) { 
+  let sum = 0; 
+  for (let item of arrayOfNumbers) { 
+    sum = sum + item;
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let newArray = [];
+  for (evenNumbers of arrayOfNumbers) {
+    if (evenNumbers % 2 == 0) { 
+      count = newArray.push(evenNumbers)
+    }
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
